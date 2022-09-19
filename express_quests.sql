b0a76958e23a49486941858d9050d1424d8541a5
laurentsata <sataward666@gmail.com>
2022-09-19 10:13:59 +0200

    add pasword in table users

--- express_quests.sql
+++ express_quests.sql
@@ -64,11 +64,12 @@ CREATE TABLE users (
     email varchar(255) UNIQUE NOT NULL,
     city varchar(255) DEFAULT NULL,
     language varchar(255) DEFAULT NULL,
+    hashedPassword varchar(255) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 
 INSERT INTO
-  users (firstname, lastname, email, city, language)
+  users (firstname, lastname, email, city, language, hashedPassword)
 VALUES
   (
     'John',
@@ -76,6 +77,7 @@ VALUES
     'john.doe@example.com',
     'Paris',
     'English'
+    '$argon2id$v=19$m=16,t=2,p=1$emVmZXpmemZlemVmZWR6ZXplZg$rqZkhxu5YbqCGHPNrjJZpQ'
   ),
   (
     'Valeriy',
@@ -83,6 +85,7 @@ VALUES
     'valeriy.appius@example.com',
     'Moscow',
     'Russian'
+    '$argon2id$v=19$m=16,t=2,p=1$emVmemVmemZlemZ6ZnpmZQ$eSetR6KPUNAGW+q+wDadcw'
   ),
   (
     'Ralf',
@@ -90,6 +93,7 @@ VALUES
     'ralf.geronimo@example.com',
     'New York',
     'Italian'
+    '$argon2id$v=19$m=16,t=2,p=1$emVmemVmemZlemZ6ZnpmZXphZGF6ZGQ$a0bg5DZB6H6v3jjQC81DXg'
   ),
   (
     'Maria',
@@ -97,6 +101,7 @@ VALUES
     'maria.iskandar@example.com',
     'New York',
     'German'
+    '$argon2id$v=19$m=16,t=2,p=1$emVmemVmemZlenplZHpkZnpmemZlemFkYXpkZA$V1qAnJDyMuuWG7g9yoGYXA'
   ),
   (
     'Jane',
@@ -104,6 +109,7 @@ VALUES
     'jane.doe@example.com',
     'London',
     'English'
+    '$argon2id$v=19$m=16,t=2,p=1$emVmemVmemZlenplZHpkZGZ6ZnpmZXphZGF6ZGQ$VCzq45PL9t8khtc44Kk5iw'
   ),
   (
     'Johanna',
@@ -111,4 +117,5 @@ VALUES
     'johanna.martino@example.com',
     'Milan',
     'Spanish'
+    '$argon2id$v=19$m=16,t=2,p=1$emVmemVmemVmemZlenplZHpkZGZ6ZnpmZXphZGF6ZGQ$UKaGZ9hGFn/S5SBQDMe/Uw'
   );
